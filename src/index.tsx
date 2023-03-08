5e4d3d1cf37d3605e2e8e9e3b0607fac62edaf16
Lovepreet Singh <lovepreetsingh@CREDBLRMAC1379-2.local>
2023-03-08 15:50:33 +0530

    Tailwind css setup Done

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './assets/main.css';
 import App from "./pages/App";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
