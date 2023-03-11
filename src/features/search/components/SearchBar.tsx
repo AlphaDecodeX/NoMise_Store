aaf865760564d43e12504158dc54b315c0b8298c
Lovepreet Singh <lovepreetsingh@CREDBLRMAC1379-2.local>
2023-03-11 14:36:16 +0530

    Search Auto Suggestion and Dropdown configured

--- src/features/search/components/SearchBar.tsx
+++ src/features/search/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ProductService } from "../../products/services/ProductService";
 import SearchService from "../services/SearchService";
 import { productFetchResponse } from "../../products/utils/productResponse";
 
@@ -8,77 +9,88 @@ type Props = {
 
 const SearchBar: React.FC<Props> = ({ className }) => {
   const [inputValue, setInputValue] = useState("");
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedSuggestion, setSelectedSuggestion] = useState<
-    productFetchResponse | null
-  >(null);
-
+  const [searchResults, setSearchResults] = useState<productFetchResponse[]>([]);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(-1);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const searchService = new SearchService();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-    setSelectedSuggestion(null);
-    setShowDropdown(true);
-  };
-
-  const handleSuggestionClick = (suggestion: productFetchResponse) => {
-    setInputValue(suggestion.name);
-    setShowDropdown(false);
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Escape") {
-      setShowDropdown(false);
-    } else if (event.key === "Enter" && selectedSuggestion !== null) {
-      setInputValue(selectedSuggestion.name);
-      setShowDropdown(false);
-    } else if (event.key === "ArrowUp" || event.key === "ArrowDown") {
-      event.preventDefault();
-      const products: productFetchResponse[] = searchService.autoCompleteAndSuggest(inputValue);
-      const currentIndex = selectedSuggestion
-        ? products.findIndex((p) => p.name === selectedSuggestion.name)
-        : -1;
-      const nextIndex =
-        event.key === "ArrowUp"
-          ? currentIndex - 1
-          : currentIndex + 1;
-      if (nextIndex >= 0 && nextIndex < products.length) {
-        setSelectedSuggestion(products[nextIndex]);
-      } else {
-        setSelectedSuggestion(null);
-      }
+    const inputValue = event.target.value;
+    setInputValue(inputValue);
+    setSelectedItemIndex(-1); // Reset the selected item index when input value changes
+    if (inputValue) {
+      const results = searchService.autoCompleteAndSuggest(inputValue);
+      setSearchResults(results);
+      setIsDropdownVisible(true);
+    } else {
+      setSearchResults([]);
+      setIsDropdownVisible(false);
     }
   };
 
-  const suggestions = searchService.autoCompleteAndSuggest(inputValue);
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    switch (event.key) {
+      case "ArrowUp":
+        event.preventDefault();
+        setSelectedItemIndex((prevIndex) =>
+          prevIndex <= 0 ? searchResults.length - 1 : prevIndex - 1
+        );
+        break;
+      case "ArrowDown":
+        event.preventDefault();
+        setSelectedItemIndex((prevIndex) =>
+          prevIndex >= searchResults.length - 1 ? 0 : prevIndex + 1
+        );
+        break;
+      case "Enter":
+        event.preventDefault();
+        if (selectedItemIndex >= 0 && selectedItemIndex < searchResults.length) {
+          setInputValue(searchResults[selectedItemIndex].name);
+          setIsDropdownVisible(false);
+        }
+        break;
+      case "Escape":
+        event.preventDefault();
+        setIsDropdownVisible(false);
+        break;
+    }
+  };
+
+  const handleItemClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const index = parseInt(event.currentTarget.getAttribute("data-index") || "");
+    if (!isNaN(index)) {
+      setInputValue(searchResults[index].name);
+      setIsDropdownVisible(false);
+    }
+  };
 
   return (
     <div className={`relative ${className}`}>
       <input
+        type="text"
         placeholder="Search"
-        value={inputValue}
         className="p-2 rounded-l-lg border-2 border-r-0 border-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+        value={inputValue}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
-      ></input>
+      />
       <button className="bg-purple-600 text-white px-4 py-2 rounded-r-lg hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">
         Search
       </button>
-      {showDropdown && suggestions.length > 0 && (
-        <ul className="absolute z-10 bg-white w-full mt-2 border border-gray-300 rounded-b-lg shadow-lg">
-          {suggestions.map((suggestion) => (
-            <li
-              className={`p-2 cursor-pointer ${
-                selectedSuggestion && selectedSuggestion.name === suggestion.name
-                  ? "bg-gray-100"
-                  : ""
+      {isDropdownVisible && (
+        <div className="absolute top-full left-0 w-full bg-white border-2 border-gray-400 rounded-b-lg">
+          {searchResults.map((result, index) => (
+            <div
+              className={`px-2 py-1 cursor-pointer ${
+                selectedItemIndex === index ? "bg-gray-200" : ""
               }`}
-              onClick={() => handleSuggestionClick(suggestion)}
+              data-index={index}
+              onClick={handleItemClick}
             >
-              {suggestion.name}
-            </li>
+              {result.name}
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
