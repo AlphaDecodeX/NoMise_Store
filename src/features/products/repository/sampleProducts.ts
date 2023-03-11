84f722fd253ca56026268138def7e14657937dff
Lovepreet Singh <lovepreetsingh@CREDBLRMAC1379-2.local>
2023-03-11 14:44:18 +0530

    Sample Products changed

--- src/features/products/repository/sampleProducts.ts
+++ src/features/products/repository/sampleProducts.ts
@@ -20,32 +20,32 @@ const products: productFetchResponse[] = [
         img: "https://www.lg.com/in/images/monitors/md07510590/32UN880-B-Thumb-350.jpg" 
     },
     {
-        name: "LG Ultragear 24inch monitor",
-        description: "LG 24Gn650 Ultragear Gaming 24 Inch (60 Cm) LCD 1920 x 1080 Pixels IPS Full Hd - 144Hz, 1Ms, G-Sync Compatible, Freesync Premium, Srgb 99%, Hdmi X 2, Display Port, HP Out, HDR 10 (Black)",
+        name: "LG Ultrawide 49-Inch Curved",
+        description: "LG 49WL95C 49-Inch Curved 32: 9 Ultrawide Dqhd IPS with HDR10 and USB Type-C,49 Inch Curved - 32:9 DQHD Resolution",
         price: 25000,
         rating: 4.3,
         offer: "25% Off",
         numberOfRatings: 200,
-        img: "https://www.lg.com/in/images/monitors/md07510590/32UN880-B-Thumb-350.jpg"
+        img: "https://m.media-amazon.com/images/I/71EP7F-yPKL._SX679_.jpg"
     },
     {
-        name: "LG Ultragear 24inch monitor",
-        description: "LG 24Gn650 Ultragear Gaming 24 Inch (60 Cm) LCD 1920 x 1080 Pixels IPS Full Hd - 144Hz, 1Ms, G-Sync Compatible, Freesync Premium, Srgb 99%, Hdmi X 2, Display Port, HP Out, HDR 10 (Black)",
+        name: "LG Ultrawide 49-Inch Curved",
+        description: "LG 49WL95C 49-Inch Curved 32: 9 Ultrawide Dqhd IPS with HDR10 and USB Type-C,49 Inch Curved - 32:9 DQHD Resolution",
         price: 25000,
         rating: 4.3,
         offer: "25% Off",
         numberOfRatings: 200,
-        img: "https://www.lg.com/in/images/monitors/md07510590/32UN880-B-Thumb-350.jpg"
-    },
+        img: "https://m.media-amazon.com/images/I/71EP7F-yPKL._SX679_.jpg"
+    } ,
     {
-        name: "LG Ultragear 24inch monitor",
-        description: "LG 24Gn650 Ultragear Gaming 24 Inch (60 Cm) LCD 1920 x 1080 Pixels IPS Full Hd - 144Hz, 1Ms, G-Sync Compatible, Freesync Premium, Srgb 99%, Hdmi X 2, Display Port, HP Out, HDR 10 (Black)",
+        name: "LG Ultrawide 49-Inch Curved",
+        description: "LG 49WL95C 49-Inch Curved 32: 9 Ultrawide Dqhd IPS with HDR10 and USB Type-C,49 Inch Curved - 32:9 DQHD Resolution",
         price: 25000,
         rating: 4.3,
         offer: "25% Off",
         numberOfRatings: 200,
-        img: "https://www.lg.com/in/images/monitors/md07510590/32UN880-B-Thumb-350.jpg"
-    }
+        img: "https://m.media-amazon.com/images/I/71EP7F-yPKL._SX679_.jpg"
+    }  
 ];
 
 export default products;
