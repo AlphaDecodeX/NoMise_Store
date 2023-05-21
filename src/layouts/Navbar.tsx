a75d24b3ad17368ea65b7f8694f1544859fa5541
Lovepreet Singh <lovepreetsingh@CREDBLRMAC1379-2.local>
2023-05-22 02:06:18 +0530

    Some Fixes

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -5,7 +5,6 @@ import "./Navbar.css";
 
 const NavBar: React.FC = ()=>{
     const imageUrl: string = "https://api.logomaster.ai/icon/abstract_1552992.svg";
-    const classForLogo: string = "h-14 w-14 p-3";
     const classForSearchBar: string = "p-4";
     return(
         <div className="space-between p-b-5 p-t-5 nav-container">
@@ -13,12 +12,8 @@ const NavBar: React.FC = ()=>{
                 <Logo imgUrl={imageUrl} className = "m-l-15"/>
                 <div className="title m-l-5">NoMise Store</div>
             </div>
-            <div className="joined-div p-r-5 m-r-5">
-                <i className="search-icon m-i-5 fa fa-search"></i>
-                <input placeholder="Search" className="search-input"></input>
-            </div>
             
-            {/* <SearchBar className = {classForSearchBar}/> */}
+            <SearchBar className = {classForSearchBar}/>
         </div>
     );
 }
