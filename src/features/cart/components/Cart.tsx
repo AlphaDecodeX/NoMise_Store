5aaae81cff9fa8d580b4d48af741dc79539f1175
Saurav Pant <pantsaurav005@gmail.com>
2023-05-29 21:36:06 +0530

    PageNotFound Added

--- src/features/cart/components/Cart.tsx
+++ src/features/cart/components/Cart.tsx
@@ -4,8 +4,10 @@ import productFetchResponse from "../../products/utils/productResponse";
 import { CartService } from "../services/cartService";
 import "./Cart.css";
 import { ProductService } from "../../products/services/ProductService";
+import { useLocation } from "react-router-dom";
 
 const Cart: React.FC = () => {
+  const location = useLocation();
   const [isCartOpen, setIsCartOpen] = useState(false);
   const cartItemsFromRedux = useSelector((state: any) => state.cart.cartItems);
   const [cartItems, setCartItems] = useState<productFetchResponse[]>([]);
@@ -52,6 +54,7 @@ const Cart: React.FC = () => {
           </span>
         ) : null}
       </div>
+
       <button
         onClick={toggleCart}
         className="medium-shadow bg-gray-800 text-white px-2 py-2 rounded-full"
@@ -59,8 +62,10 @@ const Cart: React.FC = () => {
         <img
           className="cart-image"
           src="https://www.citypng.com/public/uploads/preview/hd-shopping-cart-white-logo-icon-transparent-png-11640441682ecem2ohejv.png"
+          alt="Cart"
         />
       </button>
+
       {isCartOpen && (
         <div className="absolute bottom-10 right-0 w-80 bg-white p-6 rounded-lg shadow-lg">
           <h2 className="text-2xl font-bold mb-4">Cart</h2>
